543ba186d676b2496cd12e9a635632f397f1b88d
Hansen@DESKTOP-P1ST9AA <hansennygard@gmail.com>
2018-03-01 12:43:53 +0100

    Main limpio

--- src/pro_proyecto_2ev/PRO_Proyecto_2ev.java
+++ src/pro_proyecto_2ev/PRO_Proyecto_2ev.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pro_proyecto_2ev;
 
 /**
  *
- * @author Hansen
+ * @author abrandarizdominguez & ssahuquilloembade
  */
 public class PRO_Proyecto_2ev {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
