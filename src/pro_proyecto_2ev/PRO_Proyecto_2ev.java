5461dc7e078ec528565d722f88d084737d20bfe4
Hansen@DESKTOP-P1ST9AA <hansennygard@gmail.com>
2018-03-15 13:35:51 +0100

    Doble menu implementado (pruebas)

--- src/pro_proyecto_2ev/PRO_Proyecto_2ev.java
+++ src/pro_proyecto_2ev/PRO_Proyecto_2ev.java
@@ -10,20 +10,7 @@ public class PRO_Proyecto_2ev {
 
     public static void main(String[] args) {
         MetodosPregunta obj1 = new MetodosPregunta();
-        int opcion = Integer.parseInt(JOptionPane.showInputDialog(
-                "****** Menu *****\n"
-                + "1) Añadir pregunta\n"
-                + "2) Leer Pregunta\n"
-                + "3) Salir"));
-        do {
-            switch(opcion) {
-                case 1: obj1.menuAñadir();
-                    break;
-                case 2:
-                case 3: System.exit(0);
-                    
-            }
-        } while (opcion > 3);
+        obj1.menuPrincipal();
     }
     
 }
