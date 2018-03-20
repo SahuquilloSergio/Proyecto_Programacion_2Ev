31b5875f3c33b0b0aaeaec0b210287439c1be6bb
Hansen <Hansen@Hansen-PC>
2018-03-20 23:57:08 +0100

    Comentarios añadidos

--- src/com/adriansergio/jugador/Turno.java
+++ src/com/adriansergio/jugador/Turno.java
@@ -5,58 +5,53 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
 public class Turno {
-     int num;
+
+    int num;
     
-        public void SonidoAcertarPreguntaNormal(){
-            
-           try {
-            
+    /**
+     * Método que reproduce un sonido cuando se acierta una pregunta normal.
+     */
+    public void SonidoAcertarPreguntaNormal() {
+
+        try {
             // Se obtiene un Clip de sonido
             Clip sonido = AudioSystem.getClip();
-            
             // Se carga con un fichero wav
             sonido.open(AudioSystem.getAudioInputStream(new File("C:\\Users\\serxa\\Downloads\\MarioCoin.wav")));
-            
             // Comienza la reproducción
             sonido.start();
-            
             // Espera mientras se esté reproduciendo.
-            while (sonido.isRunning())
+            while (sonido.isRunning()) {
                 Thread.sleep(1000);
-            
+            }
             // Se cierra el clip.
             sonido.close();
         } catch (Exception e) {
             System.out.println("" + e);
         }
     }
-        
-        public void SonidoFallarPregunta(){
-                        
-           try {
-            
+    
+    /**
+     * Método que hace sonar un sonido cuando se falla una pregunta.
+     */
+    public void SonidoFallarPregunta() {
+
+        try {
             // Se obtiene un Clip de sonido
             Clip sonido = AudioSystem.getClip();
-            
             // Se carga con un fichero wav
             sonido.open(AudioSystem.getAudioInputStream(new File("C:\\Users\\serxa\\Downloads\\Error.wav")));
-            
             // Comienza la reproducción
             sonido.start();
-            
             // Espera mientras se esté reproduciendo.
-            while (sonido.isRunning())
+            while (sonido.isRunning()) {
                 Thread.sleep(1000);
-            
+            }
             // Se cierra el clip.
             sonido.close();
         } catch (Exception e) {
             System.out.println("" + e);
         }
-        }
-        
-            
-           
-        
-        
+    }
+
 }
